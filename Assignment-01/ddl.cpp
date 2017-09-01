92fa16e3399772c663854207f947b6d4945f4e77
Daniel Watkins <Daniel Watkins>
2017-09-01 00:33:05 -0500

    All operations implemented

--- Assignment-01/ddl.cpp
+++ Assignment-01/ddl.cpp
@@ -1,4 +1,4 @@
-// Filename: DoubleLinkedList.cpp
+// Filename: ddl.cpp
 //
 // Author: Dan Watkins (dwatkins@wwzard.com)
 //
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-// define nullptr for older versions of cplusplus
+// define nullptr for older versions of C++
 #ifndef nullptr
 #define nullptr 0
 #endif
@@ -35,6 +35,9 @@ int main()
 	ListNode *prevPtr = nullptr;
 	ListNode *nextPtr = nullptr;
 
+	int val, elementKey;
+	bool bKeyFound = false;
+
 	// Action character
 	char ch = 'q';
 
@@ -54,132 +57,261 @@ int main()
 		cin >> ch;
 
 		switch (ch) {
-		case 'a':
-			cout << "adding\n";
-			cout << "enter an integer ";
-			int val;
-			cin >> val;
+			case 'i':
+				// i - insert at beginning of list
+				// make new node new head, next is former head
+				// (if there was one)
+				//
+				cout << "inserting at beginning of list\n";
+				cout << "enter an integer ";
+				cin >> val;
 
-			newNode = new ListNode;
-			newNode->value = val;
-			newNode->next = nullptr;
+				newNode = new ListNode;
+				newNode->value = val;
+				newNode->next = nullptr;
+				newNode->previous = nullptr;
 
-			if (head == nullptr) {
-				head = newNode;
-			}
-			else {
-				nodePtr = head;
-				while (nodePtr->next != nullptr) {
-					cout << "hello" << endl;
-					nodePtr = nodePtr->next;
-				}
-				nodePtr->next = newNode;
-			}
-
-			cout << "head elem: " << head->value << "\n";
-			break;
-
-		case 'i':
-			cout << "inserting\n";
-			cout << "enter an integer ";
-			cin >> val;
-
-			newNode = new ListNode;
-			newNode->value = val;
-			newNode->next = nullptr;
-
-			if (head == nullptr) {
-				head = newNode;
-			}
-			else {
-				nodePtr = head;
-				while (nodePtr->next != nullptr && nodePtr->value < val) {
-					prevPtr = nodePtr;
-					nodePtr = nodePtr->next;
-				}
-				if (prevPtr == nullptr) {
+				// if empty list, new node becomes both head and tail
+				if (head == nullptr) {
 					head = newNode;
-					newNode->next = nodePtr;
-				}
-				else if (nodePtr->value > val) {
-					prevPtr->next = newNode;
-					newNode->next = nodePtr;
+					tail = newNode;
 				}
 				else {
-					nodePtr->next = newNode;
+					nodePtr = head;
+					head = newNode;
+					head->next = nodePtr;
+					nodePtr->previous = head;
 				}
-			}
 
-			cout << "head elem: " << head->value << "\n";
-			break;
+				break;
+			
+			case 'd':
+				// d - delete from beginning of list
+				//
+				cout << "deleting item from beginning of list\n";
 
-		case 'd':
-			cout << "deleting\n";
-			cout << "enter a value to be deleted ";
-			cin >> val;
+				if (head == nullptr) {
+					cout << "Empty list, nothing to delete\n";
+				}
+				else if (head->next == nullptr) {
+					// delete, but list is empty
+					delete head;
+					head = nullptr;
+					tail = nullptr;
+				}
+				else {
+					nodePtr = head->next;
+					delete head;
+					head = nodePtr;
+					nodePtr->previous = nullptr;
+				}
 
-			if (head == nullptr) {
-				return 0;
-			}
+				break;
 
-			if (head->value == val) {
-				nodePtr = head->next;
-				delete head;
-				head = nodePtr;
-			}
-			else {
+			case 'a':
+				// a - insert last - add element to end of list
+				// make node new tail, previous is old tail, if
+				// there was one
+				//
+				cout << "adding to end of list\n";
+				cout << "enter an integer ";
+				cin >> val;
+
+				newNode = new ListNode;
+				newNode->value = val;
+				newNode->next = nullptr;
+				newNode->previous = nullptr;
+
+				// if empty list, new node becomes both head and tail
+				if (head == nullptr) {
+					head = newNode;
+					tail = newNode;
+				}
+				else {
+					nodePtr = tail;
+					tail = newNode;
+					tail->previous = nodePtr;
+					nodePtr->next = tail;
+				}
+
+				break;
+
+			case 'D':
+				// D - delete from end of list
+				//
+				cout << "deleting item from end of list\n";
+
+				if (tail == nullptr) {
+					cout << "Empty list, nothing to delete\n";
+				}
+				else if (tail->previous == nullptr) {
+					// delete, but list is empty
+					delete tail;
+					head = nullptr;
+					tail = nullptr;
+				}
+				else {
+					nodePtr = tail->previous;
+					delete tail;
+					tail = nodePtr;
+					nodePtr->next = nullptr;
+				}
+
+				break;
+
+			case 'I':
+				// I - insert after an element by value
+				//
+				cout << "inserting at after an element\n";
+				cout << "enter an integer to insert: ";
+				cin >> val;
+				cout << "\nEnter the value to insert after: ";
+				cin >> elementKey;
+
+				// Walk thru list searching for key to insert after
 				nodePtr = head;
-				while (nodePtr != nullptr && nodePtr->value != val) {
-					prevPtr = nodePtr;
+
+				bKeyFound = false;
+
+				while (nodePtr != nullptr) {
+					if (nodePtr->value == elementKey) {
+						bKeyFound = true;
+
+						newNode = new ListNode;
+						newNode->value = val;
+						newNode->next = nullptr;
+						newNode->previous = nullptr;
+
+						nextPtr = nodePtr->next;
+
+						if (nextPtr) {
+							newNode->next = nextPtr;
+						}
+						else {
+							// new node is new tail
+							tail = newNode;
+						}
+
+						nodePtr->next = newNode;
+						nextPtr->previous = newNode;
+						newNode->previous = nodePtr;
+						break; // out of while loop
+					}
+
 					nodePtr = nodePtr->next;
 				}
 
-				if (nodePtr->value == val) {
-					if (nodePtr->next) {
-						prevPtr->next = nodePtr->next;
-					}
-					else {
-						prevPtr->next = nullptr;
-					}
-
-					delete nodePtr;
+				if (!bKeyFound) {
+					cout << "Key not found, no insert\n";
 				}
+
+				break;
+
+			case 'r':
+				// r - delete using key ("remove")
+				//
+				cout << "deleting (\"removing\") and element by key\n";
+				cout << "Please enter the key of the element to remove\n";
+				cin >> val;
+
+				// Walk thru list searching for key to insert after
+				nodePtr = head;
+
+				bKeyFound = false;
+
+				while (nodePtr != nullptr) {
+					if (nodePtr->value == val) {
+						bKeyFound = true;
+						prevPtr = nodePtr->previous;
+						nextPtr = nodePtr->next;
+
+						if (prevPtr == nullptr && nextPtr == nullptr) { // deleting last element in list
+							head = nullptr;
+							tail = nullptr;
+						}
+						else if (prevPtr == nullptr) { // Element was head
+							head = nextPtr;
+							nextPtr->previous = nullptr;
+						}
+						else if (nextPtr == nullptr) { // Element was tail
+							tail = prevPtr;
+							prevPtr->next = nullptr;
+						}
+						else {
+							nextPtr->previous = prevPtr;
+							prevPtr->next = nextPtr;
+						}
+
+						break; // out of while loop
+					}
+
+					nodePtr = nodePtr->next;
+				}
+
+				if (!bKeyFound) {
+					cout << "Key not found, no insert\n";
+				}
+
+				break;
+
+			case 'p':
+				// p - display forward
+				// walk the list from head forward
+				//
+				if (head == nullptr) {
+					cout << "No items in list\n";
+				}
+				else {
+					nodePtr = head;
+
+					while (nodePtr) {
+						cout << "elem: " << nodePtr->value << "\n";
+						nodePtr = nodePtr->next;
+					}
+				}
+
+				break;
+
+			case 'P':
+				// P - display backward
+				// walk the list from tail backward
+				//
+				if (tail == nullptr) {
+					cout << "No items in list\n";
+				}
+				else {
+					nodePtr = tail;
+
+					while (nodePtr) {
+						cout << "elem: " << nodePtr->value << "\n";
+						nodePtr = nodePtr->previous;
+					}
+				}
+
+				break;
+
+			case 'q':
+				cout << "quitting\n";
+
+				// free memory allocated
+				//
+				nodePtr = head;
+
+				while (nodePtr != nullptr) {
+					prevPtr = nodePtr->next;
+					delete nodePtr;
+					nodePtr = prevPtr;
+				}
+
+				head = nullptr;
+				break;
+
+				default:
+					// Unrecognized action entered
+					cout << "Try again\n";
+					break;
 			}
-
-			break;
-
-		case 'p':
-			cout << "printing ...\n";
-
-			nodePtr = head;
-
-			while (nodePtr) {
-				cout << "elem: " << nodePtr->value << "\n";
-				nodePtr = nodePtr->next;
-			}
-
-			break;
-
-		case 'q':
-			cout << "quitting\n";
-
-			while (nodePtr != nullptr) {
-				prevPtr = nodePtr->next;
-				delete nodePtr;
-				nodePtr = prevPtr;
-			}
-
-			head = nullptr;
-			break;
-
-
-		default:
-			cout << "Try again\n";
-			break;
-		}
 	} while (ch != 'q');
 
-	// system("pause"); - Windows only extension
 	return 0;
 }
-
